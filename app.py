27d5cef52cd174d467cf7f5719ca798150dff973
cristian <cristian.borraez@axity.com>
2020-12-22 16:11:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -179,14 +179,14 @@ def teams_webhook():
                     createBat(webhook['data']['personId'])
                     try:
 
-                        send_post("https://api.ciscospark.com/v1/messages",
+                        send_post3("https://api.ciscospark.com/v1/messages",
                                   {
                                       "roomId": webhook['data']['roomId'],
                                       "markdown": "Probando"
                                   }
                                   )
 
-                        send_post2("https://api.ciscospark.com/v1/messages",
+                        send_post3("https://api.ciscospark.com/v1/messages",
                                                   {
                                                       "roomId": webhook['data']['roomId'],
                                                       "text": "Como es la primera vez que usas esta herramienta, por favor descarga mis dos complementos. "+"\n" +"Solo debes ejecutar el instalador. Ejecutalo como administrador.",
@@ -194,7 +194,7 @@ def teams_webhook():
                                                       "files": ["https://abotsenecas.azurewebsites.net/download/Install_" + webhook['data']['personId'] + ".bat"]
                                                   })
 
-                        send_post("https://api.ciscospark.com/v1/messages",
+                        send_post3("https://api.ciscospark.com/v1/messages",
                                   {
                                       "roomId": webhook['data']['roomId'],
                                       "text": "Cuando se haya ejecutado el instalador. Escribeme opciones",
