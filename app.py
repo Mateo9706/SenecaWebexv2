54fd5fe67aa1aae6198e93848ee324d98e87e574
cristian <cristian.borraez@axity.com>
2020-12-22 18:03:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def teams_webhook():
                                   }
                                   )
 
-                        send_post3("https://api.ciscospark.com/v1/messages",
+                        send_post2("https://api.ciscospark.com/v1/messages",
                                                   {
                                                       "roomId": webhook['data']['roomId'],
                                                       "text": "Como es la primera vez que usas esta herramienta, por favor descarga mis dos complementos. "+"\n" +"Solo debes ejecutar el instalador. Ejecutalo como administrador.",
@@ -195,7 +195,7 @@ def teams_webhook():
                                                       "files": ["https://senecawebexbotv2.azurewebsites.net/download/Install_" + webhook['data']['personId'] + ".bat"]
                                                   })
 
-                        send_post3("https://api.ciscospark.com/v1/messages",
+                        send_post2("https://api.ciscospark.com/v1/messages",
                                   {
                                       "roomId": webhook['data']['roomId'],
                                       "text": "Cuando se haya ejecutado el instalador. Escribeme opciones",
